15dac929dee1a771874bd4f30cad1d1f9f524118
Soliana Getachew <solianag1@gmail.com>
2025-07-02 01:13:24 -0400

    docs folder added

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BudgetManager"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MyCustomException"},{"p":"<Unnamed>","l":"SavingsCalculator"}];updateSearchResults();
